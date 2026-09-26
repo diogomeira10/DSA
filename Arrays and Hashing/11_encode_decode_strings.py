ae65c7cd2edc4ca36e2a221bfa4fec148d687b2f
Diogo Meira <diogomeira81@gmail.com>
2026-09-26 19:51:57 +0100

    encode and decode solution

--- Arrays and Hashing/11_encode_decode_strings.py
+++ Arrays and Hashing/11_encode_decode_strings.py
@@ -2,45 +2,101 @@ from typing import List
 
 
 class Solution:
-
     def encode(self, strs: List[str]) -> str:
-        encoded_string = ""
-        return encoded_string
+
+        if not strs:
+            return ""
+
+        res = []
+
+        for string in strs:
+            res.append(str(len(string)))
+            res.append("#")
+            res.append(string)
+
+        return "".join(res)
 
     def decode(self, s: str) -> List[str]:
-        decoded_strings = ["",""]
-        return decoded_strings
+
+        if not s:
+            return []
+
+        strings = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            
+            print(f"j{j}")
+            print(f"s{s[j]}")
+
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[j - 1])
+            j += 1
+            word = s[j : j+length]
+            strings.append(word)
+            print(word)
+            print(f"length: {length} ")
+            i = j +length
+
+        return strings
 
 
 instance = Solution()
+
+
 def encode_and_decode(strings: List[str]) -> List[str]:
     encoded_string = instance.encode(strings)
-    print(f"Encoded string: ", encoded_string)
+    print("Encoded string: ", encoded_string)
     decoded_strings = instance.decode(encoded_string)
-    print(f"Decoded string: {decoded_strings}")
+    # print(f"Decoded string: {decoded_strings}")
     return decoded_strings
 
 
-# Test cases
-test_cases = [["Hello","World"],[""]]
+test_cases = {
+    1: ["Hello", "World"],
+    2: [""]
+}
 
-function_results = dict()
-for i,test_case in enumerate(test_cases):
+tests_failed = 0
+tests_passed = 0
+
+function_results = {}
+for test_case in test_cases.values():
     try:
-        function_results[i] = encode_and_decode(test_case)
+        encoded_decoded = encode_and_decode(test_case)
     except ValueError as err:
         print(f"FAILED TO encode_and_decode strs:{test_case} because:\n {err}")
         continue
+
+    if encoded_decoded == test_case:
+        tests_passed += 1
+    else:
+        tests_failed += 1
+
 print(f"Test Cases: {test_cases}")
 
-test_results = {"passed": 0,"failed":0}
 
-for input,output in function_results.items():
-    if input == output:
-        test_results["passed"] += 1
-    else:
-        test_results["failed"] += 1
+print(f"{tests_passed} tests passed!")
+print(f"{tests_failed} tests failed!")
 
-print(f"{test_results.get("passed")} tests passed!")
-print(f"{test_results.get("failed")} tests failed!")
+# A good software design is the one that can be changed easily
 
+
+###Recursive
+# Onde e que estou
+# Fazer o minimo passo possivel para atingir onde quero estar
+# Ver se parti alguma coisa e corrigir.
+# Repete
+
+f = "2"
+if f.isdigit():
+    print("yes")
+else:
+    print("No")
+
+
+sss = "DiogoMeira"
+print(sss[1:3])
